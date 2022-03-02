1a912f88d9b0042a1c16e16cd6b10809c7088b10
enzo goulart <enzoo.lu@gmail.com>
2022-03-02 10:46:02 -0300

    começando funçao para apliar as imagens

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+let imgPequena = document.querySelectorAll('.img_pequena')
+let imgAmpliada = document.getElementById('img_ampliada')
+
 function apliarImg() {
-    let imgPequena = document.querySelectorAll('.img_pequena')
-    let imgAmpliada = document.getElementById("img_ampliada")
-    imgPequena.innerHTML = imgAmpliada
+    
 }
