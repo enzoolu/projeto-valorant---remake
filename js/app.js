cc729540cf0df35a330d3e5a020ca2c7a0d42577
enzo goulart <enzoo.lu@gmail.com>
2022-03-01 21:21:35 -0300

    tentando ampliar a imagem

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function apliarImg() {
+    let imgPequena = document.querySelectorAll('.img_pequena')
+    let imgAmpliada = document.getElementById("img_ampliada")
+    imgPequena.innerHTML = imgAmpliada
+}
